504510f8058f66d1d893cf407af30acc7ab504b8
Lex Quarkie <lex.quarkie@gmail.com>
2026-05-31 17:53:18 +0200

    Apply automated code formatting to Error HTML and JSON controller tests

--- test/enterprise_shop_web/controllers/error_html_test.exs
+++ test/enterprise_shop_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule EnterpriseShopWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(EnterpriseShopWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(EnterpriseShopWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
--- test/enterprise_shop_web/controllers/error_json_test.exs
+++ test/enterprise_shop_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule EnterpriseShopWeb.ErrorJSONTest do
   use EnterpriseShopWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert EnterpriseShopWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert EnterpriseShopWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
